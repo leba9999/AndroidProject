212a27d3d40f13b8d4e5d5ccedc304bef75fe040
Janne Lahteenmaki <janne.lahteenmaki@metropolia.fi>
2021-02-17 12:19:18 +0200

    -Buttonit lisätty -Uudet aktiviteetit lisätty -Manifestia muokattu että pääsee takaisin MainActivityyn

---
+++ app/src/main/java/com/example/pefproject/OldRecordActivity.java
@@ -0,0 +1,14 @@
+package com.example.pefproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OldRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_old_record);
+    }
+}
